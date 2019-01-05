509ebd4a62bec312a6adacc43a3da1adf6eb4a87
Billy WEI <kurospade@gmail.com>
2019-01-05 21:55:57 +0100

    change type

--- rest-api/api-bob/api-bob.go
+++ rest-api/api-bob/api-bob.go
@@ -154,12 +154,17 @@ func postLedState(w http.ResponseWriter,r*http.Request) {
 		fmt.Println("Valeur: ", response.Results[0])
 		if (len(response.Results[0].Series) == 0) {
 			led_fields= map[string]interface{}{
-				"value": true,
+				"value": 1,
 			}
 		} else {
-			if val, ok := response.Results[0].Series[0].Values[0][1].(bool); ok {
+			if val, ok := response.Results[0].Series[0].Values[0][1].(int); ok {
+				if (val == 1) {
+					val = 0
+				} else {
+					val = 1
+				}
 				led_fields= map[string]interface{}{
-					"value": !val,
+					"value": val,
 				}
 			} else {
 				log.Fatal(ok)
